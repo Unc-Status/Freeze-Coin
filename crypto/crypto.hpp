c89e5666604cc0da2f2e7ee961f772ddaef10309
UncStatus++ <164366136+Unc-Status@users.noreply.github.com>
2025-12-10 13:12:41 -0600

    Create crypto.hpp

---
+++ crypto/crypto.hpp
@@ -0,0 +1,16 @@
+/*crypto.hpp*/
+
+#ifndef _CRYPTO_HPP_
+#define _CRYPTO_HPP_
+
+namespace frzCrypto {
+
+   class Coin {
+      public:
+       Coin();
+       virtual ~Coin();
+   };
+
+}
+
+#endif
